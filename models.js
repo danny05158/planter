a6ece6a73fe4758c6a410ab33677d420f6a31123
Daniel Casasola <casasolad86@gmail.com>
2018-09-24 16:43:00 -0500

    bulk create used in seed.js

--- models.js
+++ models.js
@@ -1,6 +1,5 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/plantr');
-module.exports = db;
 
 const Gardner = db.define('gardner', {
   name: Sequelize.STRING,
@@ -22,3 +21,30 @@ Plot.belongsTo(Gardner);
 Gardner.belongsTo(Vegetable, { as: 'favorite_vegetable' });
 Vegetable.belongsToMany(Plot, { through: 'vegetable_plot' });
 Plot.belongsToMany(Vegetable, { through: 'vegetable_plot' });
+
+module.exports = {db, Gardner, Plot, Vegetable}
+
+// const veg1 = Vegetable.create({
+//   name: 'tomato',
+//   color: 'red',
+//   planted_on: new Date()
+// })
+// .then(veg =>{
+//   console.log('vegetable created')
+// })
+
+// Vegetable.bulkCreate([
+//   { name: 'tomato', color: 'red', planted_on: new Date()},
+//   { name: 'lettuce', color: 'green', planted_on: new Date()},
+//   { name: 'carrot', color: 'orange', planted_on: new Date()},
+//   { name: 'zucchini', color: 'green', planted_on: new Date()},
+// ]).then( veg => { // Notice: There are no arguments here, as of right now you'll have to...
+//    Gardner.bulkCreate([
+//      {name: 'John', age: 30, favoriteVegetableId: Vegetable.id},
+//      {name: 'Matt', age: 31, favoriteVegetableId: Vegetable.id},
+//      {name: 'Jon', age: 32, favoriteVegetableId: Vegetable.id},
+//      {name: 'Joe', age: 33, favoriteVegetableId: Vegetable.id},
+
+//    ])
+//   console.log('vegetable created')
+// })
--- seed.js
+++ seed.js
@@ -1,11 +1,40 @@
-const db = require('./models');
+// const db = require('./models');
 
-db.sync({ force: true })
-  .then(() => {
-    console.log('Database Synced');
-    db.close();
+const {db, Gardner, Plot, Vegetable} = require('./models')
+
+// Vegetable.bulkCreate([
+//   { name: 'tomato', color: 'red', planted_on: new Date()},
+//   { name: 'lettuce', color: 'green', planted_on: new Date()},
+//   { name: 'carrot', color: 'orange', planted_on: new Date()},
+//   { name: 'zucchini', color: 'green', planted_on: new Date()},
+// ]).then( veg => { // Notice: There are no arguments here, as of right now you'll have to...
+//   console.log('vegetable created')
+// })
+
+db.sync({force: true})
+.then(() => {
+  Vegetable.bulkCreate([
+    { name: 'tomato', color: 'red', planted_on: new Date()},
+    { name: 'lettuce', color: 'green', planted_on: new Date()},
+    { name: 'carrot', color: 'orange', planted_on: new Date()},
+    { name: 'zucchini', color: 'green', planted_on: new Date()},
+  ])
+  .then( (veg) => { // Notice: There are no arguments here, as of right now you'll have to...
+     Gardner.bulkCreate([
+       {name: 'John', age: 30, favoriteVegetableId: Vegetable.id},
+       {name: 'Matt', age: 31, favoriteVegetableId: Vegetable.id},
+       {name: 'Jon', age: 32, favoriteVegetableId: Vegetable.id},
+       {name: 'Joe', age: 33, favoriteVegetableId: Vegetable.id},
+      ])
+  })
+})
+.then( () => {
+
+  console.log('Database Synced');
+    // db.close();
+
   })
   .catch(() => {
-    console.log('Something went wrong!');
+     console.log('Something went wrong!');
     db.close();
   });
